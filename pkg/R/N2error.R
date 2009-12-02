002bbbb0a6a33dc58a1c1bb2a41ef9ef4e66e6a8
thoree (Thore Egeland) <thore.egeland@medisin.uio.no>
2009-12-02 08:46:20 +0000

    Some typos corrected
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/forensim@188 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/N2error.R
+++ pkg/R/N2error.R
@@ -2,7 +2,7 @@ N2error <-
 function(dat){
   nMarkers=dim(dat)[2]-1
   error=1
-  names(error)=c("Max allele Cont error")
+  names(error)=c("Max allele Count error")
   for (i in 1:nMarkers){
     p=dat[!is.na(dat[,i+1]),i+1]
     error=error*sum(N2Exact(p)[1:2])
