0511beb694b8ee65a3236e541d815e8bc6ed60e2
hindahaned (Hinda Haned) <hindantation@gmail.com>
2010-05-24 09:48:17 +0000

    formula corrected
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/forensim@243 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DNAproxy.R
+++ pkg/R/DNAproxy.R
@@ -1,85 +1,121 @@
-#Hinda Haned, November 2009
-DNAproxy<-function(tab,x)
-{
-	#if tab is generated form DNA mixtures, then the number of columns must be 6
-	#this a temporary way to test for mixtures, to be improved !
-	if(length(tab[[1]])==6)
-	{
-		mix<-TRUE
-	}
-	else{mix<-FALSE}
-	DNAproxy.loc<-function(tabloc,x)
-	{
-		I<-intersect(tabloc$Expected,tabloc$Observed)
-		if(length(I)!=0)
-		{
-			n<-rep(0,length(I))
-			names(n)<-I
-			H<-tabloc[,"Heights"]
-			names(H)<-tabloc[,"Expected"]
-			h<-n; s<-n
-			if(mix)
-			{
-				#x1  are the non dropped out alleles, belonging to the first individual
-				x1<-tabloc[tabloc$Expected %in% I,x]
-				#x2  are the non dropped out alleles, belonging to the second individual
-				x2<-tabloc[tabloc$Expected %in% I,names(tabloc)[2:3][which(!(names(tabloc)[2:3]) %in% x)]]
-				for(i in 1:length(I))
-				{	
-					h[I[i]]<-H[I[i]]#h stores the peak heights of the non dropped out alleles
-					
-					if(x1[i]==0)
-					{	n[I[i]]<-0#n records the number of hetero and homo alleles
-						s[I[i]]<-0#single contributed alleles, s give the indexes of the elements to be taken into account in the calculation
-					}
-					if(x1[i]!=0)
-					{ 
-						if(x1[i]==1 & x2[i]==0){n[I[i]]<-1; s[I[i]]<-1}
-						if(x1[i]==1 & x2[i]>1){n[I[i]]<-1; s[I[i]]<-0}
-						if(x1[i]==2 & x2[i]==0){n[I[i]]<-2; s[I[i]]<-1}
-						if(x1[i]==1 & x2[i]==1){n[I[i]]<-1; s[I[i]]<-0}
-						if(x1[i]==2 & x2[i]>1){n[I[i]]<-2; s[I[i]]<-0}
-					}
-				}
-				cbind.data.frame('deno'=sum(n*s),'num'=sum(h*s))
-			}
-			#the procedure is straightforward if the data is composed of single contributor stains
-			else
-			{
-				#all alleles are taken into account, we don't need the s vector
-				#n records the coefficients : 1 for alleles carried by a heterozygote and 2 for alleles carried by a homozygote
-				#h records the peak heights of the observed alleles
-				for(i in 1:length(I))
-				{
-					#if two alleles are expected, then all elements of n are equal to 1
-					if(length(tabloc$Expected)==2) {n[I[i]]<-1}
-					#if a single allele is expected then  length(I) is one at most, and n[I[i]]<-2
-					if(length(tabloc$Expected)==1 ){n[I[i]]<- 2}
-					h[I[i]]<-H[I[i]]
-				}
-				#sum(n): n=2 if allele carried by a homozygote, n=1 if allele carried by a heterozygote; sum(h): sum of the peak heights observed
-				cbind.data.frame('deno'=sum(n),'num'=sum(h))
-			}
-			
-		}
-
-		#if all alleles have dropped out, the locus can not be taken into account for the DNA proxies calculations
-		else#if all alleles have dropped out, the locus can not be taken into account for the DNA proxies calculations
-		{
-			cbind.data.frame('deno'=0,'num'=0)
-		}
-	}
-
-	#tabo<-org(x,y,geno,tab)
-	#DNAproxy.loc(tab[[1]],'c1')
-	ind<-sum(unlist(sapply(tab,DNAproxy.loc,x)[2,]))/sum(unlist(sapply(tab,DNAproxy.loc,x)[1,]))
-	if(is.na(ind)){ind<-0}
-	ind
+DNAproxy <- function (tab, x) {
+    if (length(tab[[1]]) == 6) {
+        mix <- TRUE
+    }
+    else {
+        mix <- FALSE
+    }
+    DNAproxy.loc <- function(tabloc, x) {
+        I <- intersect(tabloc$Expected[tabloc$D==0], tabloc$Observed[tabloc$D==0]) 
+        if (length(I) != 0) {
+            n <- rep(0, length(I))
+            names(n) <- I
+            H <- tabloc[, "Heights"]
+            names(H) <- tabloc[, "Expected"]
+            h <- n
+            s <- n
+            if (mix) {
+                x1 <- tabloc[tabloc$Expected %in% I, x]
+                x2 <- tabloc[tabloc$Expected %in% I, names(tabloc)[2:3][which(!(names(tabloc)[2:3]) %in% 
+                  x)]]
+                for (i in 1:length(I)) {
+                  h[I[i]] <- H[I[i]]
+                  if (x1[i] == 0) {
+                    n[I[i]] <- 0
+                    s[I[i]] <- 0
+                  }
+                  if (x1[i] != 0) {
+                    if (x1[i] == 1 & x2[i] == 0) {
+                      n[I[i]] <- 1
+                      s[I[i]] <- 1
+                    }
+                    if (x1[i] == 1 & x2[i] > 1) {
+                      n[I[i]] <- 1
+                      s[I[i]] <- 0
+                    }
+                    if (x1[i] == 2 & x2[i] == 0) {
+                      n[I[i]] <- 2
+                      s[I[i]] <- 1
+                    }
+                    if (x1[i] == 1 & x2[i] == 1) {
+                      n[I[i]] <- 1
+                      s[I[i]] <- 0
+                    }
+                    if (x1[i] == 2 & x2[i] > 1) {
+                      n[I[i]] <- 2
+                      s[I[i]] <- 0
+                    }
+                  }
+                }
+                cbind.data.frame(deno = sum(n * s), num = sum(h * s))
+            }
+            else {
+                for (i in 1:length(I)) {
+                  if (length(tabloc$Expected) == 2) {
+                    n[I[i]] <- 1
+                  }
+                  if (length(tabloc$Expected) == 1) {
+                    n[I[i]] <- 2
+                  }
+                  h[I[i]] <- H[I[i]]
+                }
+                cbind.data.frame(deno = sum(n), num = sum(h))
+            }
+        }
+        else {
+            cbind.data.frame(deno = 0, num = 0)
+        }
+    }
+    ind <- sum(unlist(sapply(tab, myDNAproxy.loc, x)[2, ]))/sum(unlist(sapply(tab, myDNAproxy.loc, x)[1, ]))
+    if (is.na(ind)) {
+        ind <- 0
+    }
+    ind
 }
 
-
-
-	
-
-
-
+tabDNAproxy <- function (x, y = NULL, geno, tabcsv) 
+{
+    tabo <- recordDrop(x, y, geno, tabcsv)
+    if (length(tabo[[1]]) == 6) {
+        mix <- TRUE
+    }
+    else {
+        mix <- FALSE
+    }
+    m <- names(geno)
+    perloc <- vector("list", length(m))
+    names(perloc) <- m
+    if (mix) {
+        ind1 <- myDNAproxy(tabo, paste("c", x, sep = ""))
+        ind2 <- myDNAproxy(tabo, paste("c", y, sep = ""))
+        for (M in names(tabo)) {
+            Dloc <- NULL
+            Hestim <- NULL
+            for (h in 1:nrow(tabo[[M]])) {
+                Dloc <- c(Dloc, tabo[[M]][h, "D"])
+                Hestim <- c(Hestim, sum(tabo[[M]][h, 2:3] * c(ind1, 
+                  ind2)))
+            }
+            perloc[[M]] <- cbind.data.frame(Dloc, Hestim)
+        }
+    }
+    else {
+        ind <- myDNAproxy(tabo)
+        for (M in names(tabo)) {
+            Dloc <- NULL
+            Hestim <- NULL
+            for (h in 1:nrow(tabo[[M]])) {
+                Dloc <- c(Dloc, tabo[[M]][h, "D"])
+                if (length(tabo[[M]]$Expected) == 1) {
+                  state <- 2
+                }
+                if (length(tabo[[M]]$Expected) == 2) {
+                  state <- 1
+                }
+                Hestim <- c(Hestim, sum(state * ind))
+            }
+            perloc[[M]] <- cbind.data.frame(Dloc, Hestim)
+        }
+    }
+    perloc
+}
