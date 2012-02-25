2a2d3945ea6601798bb964067319dc825051b060
hindahaned (Hinda Haned) <hindantation@gmail.com>
2012-02-25 18:46:54 +0000

    package version updated 3.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/forensim@346 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,8 +1,5 @@
-.First.lib <- function (lib, pkg){
-    library.dynam("forensim", pkg, lib)
-    pkg.version <- packageDescription("forensim", fields = "Version")
 
-    startup.txt <- paste("   ==========================\n    adegenet", pkg.version, "is loaded\n   ==========================\n\n - to start, type '?adegenet'\n - to browse adegenet website, type 'adegenetWeb()'\n - to post questions/comments: adegenet-forum@lists.r-forge.r-project.org\n\n")
-
-    packageStartupMessage(startup.txt)
+.First.lib <-function (lib, pkg) {
+library.dynam("forensim", pkg, lib)
+packageStartupMessage('## forensim 3.0 is loaded ###' )
 }
