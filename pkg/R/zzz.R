5d96aad98cd9fc6da28dae6a3e337ffb86242408
hindahaned (Hinda Haned) <hindantation@gmail.com>
2013-01-26 19:54:13 +0000

    file zzz.R removed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/forensim@420 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++
@@ -1,7 +0,0 @@
-
-.onAttach <- function(libname, pkgname){
-pkgversion <- packageDescription("forensim", fields = "Version")
-m0<-paste("## forensim ",pkgversion," is loaded ###")
-  packageStartupMessage(m0)
-}
-
